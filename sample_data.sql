a26bba5fb0d13a7fdd4bd63ec44186f75e0e7741
Lucija Topolko <53304190+LucijaTopolko@users.noreply.github.com>
2025-10-31 22:24:25 +0000

    sql

---
+++ sample_data.sql
@@ -0,0 +1,26 @@
+INSERT INTO uloge VALUES ('prvi sudac');
+INSERT INTO uloge VALUES ('drugi sudac');
+INSERT INTO uloge VALUES ('zapisnicar');
+INSERT INTO uloge VALUES ('granicni sudac');
+INSERT INTO uloge VALUES ('delegat');
+
+INSERT INTO rangovi VALUES ('gradski');
+INSERT INTO rangovi VALUES ('zupanijski');
+INSERT INTO rangovi VALUES ('nacionalni');
+INSERT INTO rangovi VALUES ('medunarodni');
+
+INSERT INTO gradovi (oznaka_grada, naziv) VALUES ('ZG', 'Zagreb');
+INSERT INTO gradovi (oznaka_grada, naziv) VALUES ('ST', 'Split');
+INSERT INTO gradovi (oznaka_grada, naziv) VALUES ('RI', 'Rijeka');
+INSERT INTO gradovi (oznaka_grada, naziv) VALUES ('OS', 'Osijek');
+INSERT INTO gradovi (oznaka_grada, naziv) VALUES ('ZD', 'Zadar');
+INSERT INTO gradovi (oznaka_grada, naziv) VALUES ('SK', 'Slavonski Brod');
+INSERT INTO gradovi (oznaka_grada, naziv) VALUES ('PU', 'Pula');
+INSERT INTO gradovi (oznaka_grada, naziv) VALUES ('KA', 'Karlovac');
+INSERT INTO gradovi (oznaka_grada, naziv) VALUES ('VA', 'Varazdin');
+INSERT INTO gradovi (oznaka_grada, naziv) VALUES ('SI', 'Sibenik');
+INSERT INTO gradovi (oznaka_grada, naziv) VALUES ('VK', 'Vinkovci');
+INSERT INTO gradovi (oznaka_grada, naziv) VALUES ('VU', 'Vukovar');
+INSERT INTO gradovi (oznaka_grada, naziv) VALUES ('KO', 'Koprivnica');
+INSERT INTO gradovi (oznaka_grada, naziv) VALUES ('CK', 'Cakovec');
+INSERT INTO gradovi (oznaka_grada, naziv) VALUES ('PO', 'Pozega');
